876331d16f0def2666db6e20b4359d4f26fc1fe7
Daniel Cady <danielcady5@gmail.com>
2016-10-01 12:10:10 -0500

    I did cool things :100:

--- sports/script1.js
+++ sports/script1.js
@@ -1,9 +1,25 @@
+
 function xc() {
-    document.getElementById("content-list").innerHTML = 
-    '<a class="links-sports" href="https://www.google.com">' + "Google" + '</a>';
+	var display = '<a class="link" href="https://google.com">' + "Google.com" + '</a>';
+	document.getElementById('content-list').innerHTML =	display;	 
 }
 
 function football() {
-	document.getElementById("content-list").innerHTML = 
-    '<a class="links-sports" href="https://www.google.com">' + "Ryan is dumb" + '</a>';
+	$('#content-list').empty();
+	var display = '<a class="link" href="https://google.com">' + "a" + '</a>';
+	var display1 = '<a class="link" href="https://google.com">' + "b" + '</a>';
+	var display2 = '<a class="link" href="https://google.com">' + "c" + '</a>';
+	var display3 = '<a class="link" href="https://google.com">' + "d" + '</a>';
+	var display4 = '<a class="link" href="https://google.com">' + "e" + '</a>';
+	var display5 = '<a class="link" href="https://google.com">' + "f" + '</a>';
+
+	var displays = [display, display1, display2, display3, display4, display5];
+
+	for (x = 0; x < displays.length; x++) {
+		$('#content-list').append(displays[x] + '<br>')
+	}
+
+	
 }
+
+
