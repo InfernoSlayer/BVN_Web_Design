0df811e1de26946972b35a0e6c31c3f444a084bd
Daniel Cady <danielcady5@gmail.com>
2016-10-01 11:07:48 -0500

    add a script file

--- sports/script1.js
+++ sports/script1.js
@@ -1,9 +1,9 @@
 function xc() {
-            document.getElementById("content-list").innerHTML = 
-            '<a class="links-sports" href="https://www.google.com">' + "Google" + '</a>';
-        }
+    document.getElementById("content-list").innerHTML = 
+    '<a class="links-sports" href="https://www.google.com">' + "Google" + '</a>';
+}
 
-        function football() {
-            document.getElementById("content-list").innerHTML = 
-            '<a class="links-sports" href="https://www.google.com">' + "Ryan is dumb" + '</a>';
-        }
+function football() {
+	document.getElementById("content-list").innerHTML = 
+    '<a class="links-sports" href="https://www.google.com">' + "Ryan is dumb" + '</a>';
+}
