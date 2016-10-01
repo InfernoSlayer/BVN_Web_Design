8ce80a5d892a22d09b468d04728f0ffdd8429b52
InfernoSlayer <glaremasters@gmail.com>
2016-10-01 15:30:05 -0500

    Updated JS

--- sports/script1.js
+++ sports/script1.js
@@ -48,4 +48,121 @@ function ggolf() {
 	
 }
 
+function gtennis() {
+	$('#content-list').empty();
+	var display = '<a class="link" href="https://google.com">' + "Varsity" + '</a>';
+	var display1 = '<a class="link" href="https://google.com">' + "Junior Varsity" + '</a>';
 
+	var displays = [display, display1];
+
+	for (x = 0; x < displays.length; x++) {
+		$('#content-list').append(displays[x] + '<br>')
+	}
+
+	
+}
+
+function gvball() {
+	$('#content-list').empty();
+	var display = '<a class="link" href="https://google.com">' + "Varsity" + '</a>';
+	var display1 = '<a class="link" href="https://google.com">' + "Junior Varsity" + '</a>';
+
+	var displays = [display, display1];
+
+	for (x = 0; x < displays.length; x++) {
+		$('#content-list').append(displays[x] + '<br>')
+	}
+
+	
+}
+
+function boysbball() {
+	$('#content-list').empty();
+	var display = '<a class="link" href="https://google.com">' + "Varsity" + '</a>';
+	var display1 = '<a class="link" href="https://google.com">' + "Junior Varsity" + '</a>';
+	var display2 = '<a class="link" href="https://google.com">' + "Sophomore" + '</a>';
+	var display3 = '<a class="link" href="https://google.com">' + "Freshmen" + '</a>';
+
+	var displays = [display, display1, display2, display3];
+
+	for (x = 0; x < displays.length; x++) {
+		$('#content-list').append(displays[x] + '<br>')
+	}
+
+	
+}
+
+function gbball() {
+	$('#content-list').empty();
+	var display = '<a class="link" href="https://google.com">' + "Varsity" + '</a>';
+	var display1 = '<a class="link" href="https://google.com">' + "Junior Varsity" + '</a>';
+	var display2 = '<a class="link" href="https://google.com">' + "Silver" + '</a>';
+	var display3 = '<a class="link" href="https://google.com">' + "Navy" + '</a>';
+
+	var displays = [display, display1, display2, display3];
+
+	for (x = 0; x < displays.length; x++) {
+		$('#content-list').append(displays[x] + '<br>')
+	}
+
+	
+}
+
+function wrestling() {
+	$('#content-list').empty();
+	var display = '<a class="link" href="https://google.com">' + "Varsity" + '</a>';
+	var display1 = '<a class="link" href="https://google.com">' + "Junior Varsity" + '</a>';
+	var display2 = '<a class="link" href="https://google.com">' + "Freshman" + '</a>';
+
+	var displays = [display, display1, display2];
+
+	for (x = 0; x < displays.length; x++) {
+		$('#content-list').append(displays[x] + '<br>')
+	}
+
+	
+}
+
+function gbowling() {
+	$('#content-list').empty();
+	var display = '<a class="link" href="https://google.com">' + "Varsity" + '</a>';
+	var display1 = '<a class="link" href="https://google.com">' + "Junior Varsity" + '</a>';
+
+	var displays = [display, display1];
+
+	for (x = 0; x < displays.length; x++) {
+		$('#content-list').append(displays[x] + '<br>')
+	}
+
+	
+}
+
+function baseball() {
+	$('#content-list').empty();
+	var display = '<a class="link" href="https://google.com">' + "Varsity" + '</a>';
+	var display1 = '<a class="link" href="https://google.com">' + "Junior Varsity" + '</a>';
+	var display2 = '<a class="link" href="https://google.com">' + "C" + '</a>';
+	var display3 = '<a class="link" href="https://google.com">' + "D" + '</a>';
+
+	var displays = [display, display1, display2, display3];
+
+	for (x = 0; x < displays.length; x++) {
+		$('#content-list').append(displays[x] + '<br>')
+	}
+
+	
+}
+
+function gsoftball() {
+	$('#content-list').empty();
+	var display = '<a class="link" href="https://google.com">' + "Varsity" + '</a>';
+	var display1 = '<a class="link" href="https://google.com">' + "Junior Varsity" + '</a>';
+
+	var displays = [display, display1];
+
+	for (x = 0; x < displays.length; x++) {
+		$('#content-list').append(displays[x] + '<br>')
+	}
+
+	
+}
