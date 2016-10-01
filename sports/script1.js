02e7710a4b3c97c7bf5f2ace9c60e83cc8a88b39
Daniel Cady <danielcady5@gmail.com>
2016-10-01 16:29:35 -0500

    buttons

--- sports/script1.js
+++ sports/script1.js
@@ -2,7 +2,18 @@
 function xc() {
 	$('#content-list').empty();
 	var display = '<a href="https://google.com"> <button>' + "Meet Information" + '</button></a>';
-	$('#content-list').append(display);
+	var display1 = '<a href="https://google.com"> <button>' + "Varsity Boys" + '</button></a>';
+	var display2 = '<a href="https://google.com"> <button>' + "Varsity Girls" + '</button></a>';
+	var display3 = '<a href="https://google.com"> <button>' + "Junior Varsity Boys" + '</button></a>';
+	var display4 = '<a href="https://google.com"> <button>' + "Junior Varsity Girls" + '</button></a>';
+	var display5 = '<a href="https://google.com"> <button>' + "C Boys" + '</button></a>';
+	var display6 = '<a href="https://google.com"> <button>' + "C Girls" + '</button></a>';
+
+	var displays = [display, display1, display2, display3, display4, display5, display6];
+
+	for (x = 0; x < displays.length; x++) {
+		$('#content-list').append(displays[x]);
+	}
 }
 
 function football() {
